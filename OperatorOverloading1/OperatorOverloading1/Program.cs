91155d52333f4d5c607d0c54849889b48d2d675e
PodiWan <38519328+PodiWan@users.noreply.github.com>
2020-09-19 21:03:47 +0300

    Add files via upload

---
+++ OperatorOverloading1/OperatorOverloading1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace OperatorOverloading1
+{
+    class Fraction
+    {
+        public int a { get; set; } //numarator
+        public int b { get; set; } //numitor
+
+        public static Fraction operator +(Fraction f1, Fraction f2)
+        {
+            Fraction result = new Fraction();
+
+            result.a = f1.a * f2.b + f2.a * f1.b;
+            result.b = f1.b * f2.b;
+
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction fraction = new Fraction();
+            Fraction fraction1 = new Fraction();
+            Fraction fraction2 = new Fraction();
+
+            fraction1.a = 1;
+            fraction1.b = 2;
+            fraction2.a = 3;
+            fraction2.b = 4;
+
+            fraction = fraction1 + fraction2;
+
+            Console.WriteLine("{0}/{1}", fraction.a, fraction.b);
+        }
+    }
+}
